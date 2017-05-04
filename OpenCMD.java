aa5f92213e699cab87473794e5811f395095073f
rohithvutnoor <rohithvutnoor@gmail.com>
2017-05-04 19:52:59 +0530

    Update OpenCommandprompt Code

--- OpenCMD.java
+++ OpenCMD.java
@@ -2,8 +2,8 @@ import java.io.IOException;
 public class OpenCMD
 {
 	public static void main(String []args){
-    public static void getch() throws IOException, InterruptedException
-    {
-        new ProcessBuilder("cmd", "/c", "pause > null").inheritIO().start().waitFor();
-    }    }
+		public static void getch() throws IOException, InterruptedException{
+        		new ProcessBuilder("cmd", "/c", "pause > null").inheritIO().start().waitFor();
+		}    
+	}
 }
