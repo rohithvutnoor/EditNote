b14441f7896095cf913d319458a90abd88a1cdbf
Rohith Vutnoor <rohithvutnoor@gmail.com>
2017-05-04 19:23:13 +0530

    Optimizing Dictionary Search Feature

--- src/alvinEditor/Dictionary.java
+++ src/alvinEditor/Dictionary.java
@@ -6,9 +6,8 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -21,8 +20,90 @@ import javax.swing.JTextField;
 public class Dictionary extends JDialog {
 
 	/**
-	* 
-	*/
+	 * Dictionary Optimized Code
+	 */
+	// Insert All Words to TrieList
+	private static void insert(TrieContainer start, String word, String meaning) {
+		word = word.trim();
+		for (int j = 0; j < word.length(); j++) {
+			char character = word.charAt(j);
+			// In series, check the position of character,
+			// if it is already filled then check the series of filled Tries
+			// object.
+			// if it is not filled then create new TrieContainer object and
+			// place it at correct position, and check
+			// if it is end of the word, then mark isEnd = true or else false;
+			if (start.childrens[character - 97] != null) {
+				if (word.length() - 1 == j) {
+					// if word is found till last character, then mark the end
+					// as true.
+					start.childrens[character - 97].isEnd = true;
+				}
+				start = start.childrens[character - 97];
+			} else {
+				TrieContainer trie = new TrieContainer();
+				trie.isEnd = (word.length() - 1 == j ? true : false);
+				// direct link to meaning
+				trie.meaning = meaning; // if stored meanings search will be
+										// O(word.length)
+				trie.word = word; // if stored it's easy to print directly
+
+				start.childrens[character - 97] = trie;
+				start = start.childrens[character - 97];
+			}
+		}
+	}
+
+	// Search for a word in TrieList
+	private static String search(TrieContainer start, String word) {
+		// boolean isFound = true;
+		String meaning = "";
+		for (int i = 0; i < word.length(); i++) {
+			char character = word.charAt(i);
+			// if at character position TrieContainer object is present then
+			// character is found and
+			// start looking for next character is word.
+			if (start.childrens[character - 97] != null) {
+				if (word.length() - 1 != i) {
+					start = start.childrens[character - 97];
+				} else {
+					if (start.childrens[character - 97].isEnd) { // !
+						// isFound = true; //false
+						meaning = start.meaning;
+					}
+				}
+			} else {
+				// isFound = false;
+				break;
+			}
+		}
+		return meaning;
+	}
+
+	// Build Dictionary into TrieList
+	private static void buildDictionary(String file, TrieContainer trieList) throws IOException {
+		String word = "";
+		String meaning = "";
+		String currentLine = "";
+		BufferedReader buffer = null;
+		buffer = new BufferedReader(new FileReader(file));
+		int line = 0;
+		while (((currentLine = buffer.readLine()) != null) && line <= 23202) {
+			if (currentLine.indexOf("_") > 0) {
+				String arr[] = currentLine.split("_");
+				word = arr[0];
+				meaning = arr[1];
+				// if(word.equalsIgnoreCase("ses"))
+				// System.out.println(meaning);
+				insert(trieList, word.toLowerCase(), meaning);
+				// System.out.println(currentLine.indexOf("_"));
+			}
+			++line;
+		}
+		buffer.close();
+	}
+
+	// Window Design
 	private static final long serialVersionUID = 1L;
 	JPanel panel;
 	JLabel label;
@@ -37,7 +118,6 @@ public class Dictionary extends JDialog {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("/image/IDictionary.png")));
 
 		panel = new JPanel();
-		;
 		label = new JLabel("Find Meaning : ");
 		meaningArea = new JTextArea(5, 50);
 		field = new JTextField(10);
@@ -70,10 +150,32 @@ public class Dictionary extends JDialog {
 
 				String textFieldValue = field.getText();
 				find = textFieldValue;
+				
+				TrieContainer trieList = new TrieContainer();
+				String filePath = "dictionaryOptimized.txt";
+				
+				try {
+					buildDictionary(filePath, trieList);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
 				if (find.length() > 0) {
+					String meaning = search(trieList, find.toLowerCase());
+					String data = find + meaning;
+					meaningArea.setText(data);
+					meaningArea.setLineWrap(true);
+					meaningArea.setWrapStyleWord(true);
+				}
+			}
+		});
 
-					InputStream is = this.getClass().getResourceAsStream("/file/dictionary.txt");
+	}
+}
 
+
+/*
+ * InputStream is = this.getClass().getResourceAsStream("/file/dictionary.txt");
 					BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 					strLine = null;
@@ -110,10 +212,4 @@ public class Dictionary extends JDialog {
 							e.printStackTrace();
 						}
 					}
-
-				}
-			}
-		});
-
-	}
-}
+ */
---
+++ src/alvinEditor/TrieContainer.java
@@ -0,0 +1,8 @@
+package alvinEditor;
+
+public class TrieContainer {
+	public boolean isEnd;
+	String meaning;
+	String word;
+	public TrieContainer[] childrens = new TrieContainer[26];
+}
